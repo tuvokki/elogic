00a4bb0a188ad0573fccbaa4f82b09157b691c74
Wouter Roosendaal <wouter.roosendaal@planonsoftware.com>
2016-10-15 09:17:48 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+        // enable additional rules
+        // "indent": ["error", 2],
+        // "semi": ["error", "always"],
+
+        // override default options for rules from base configurations
+        // "comma-dangle": ["error", "always"],
+        // "no-cond-assign": ["error", "always"],
+
+        // disable rules from base configurations
+        "no-console": "off",
+    }
+};
